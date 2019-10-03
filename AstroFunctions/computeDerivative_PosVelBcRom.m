dd7adf241c3a22a977729d0b0bdb6336f93c79cd
davidgondelach <davidgondelach@gmail.com>
2019-10-03 10:50:07 -0400

    Separate function for ROM density computation

--- AstroFunctions/computeDerivative_PosVelBcRom.m
+++ AstroFunctions/computeDerivative_PosVelBcRom.m
@@ -1,22 +1,23 @@
-function [ f ] = computeDerivative_PosVelBcRom(t,xp,AC,BC,Inp,rR,noo,svs,F_U,M_U,maxAtmAlt,et0,jdate0)
+function [ f ] = computeDerivative_PosVelBcRom(t,xp,AC,BC,SWinputs,r,noo,svs,F_U,M_U,maxAtmAlt,et0,jdate0)
 % COMPUTEDERIVATIVE_POSVELBCROM - Compute derivatives of objects
 % position, velocity and BC, and reduced-order state
 %
 % Syntax:  [ f ] = computeDerivative_PosVelBcRom(t,xp,AC,BC,Inp,rR,noo,svs,F_U,M_U,maxAtmAlt,et0,jdate0)
 %
 % Inputs:
-%   t           current time w.r.t. et0 [s]
-%   xp          current state of multiple objects and reduced order atmosphere state
-%   AC          continuous state transition matrix
-%   BC          continuous input matrix
-%   Inp         inputs (F10.7, Kp, UT, doy)
-%   rR          number of reduced order modes
-%   noo         number of objects
-%   svs         state size per object
+%   t           current time: seconds since et0 [s]
+%   xp          state vector: position and velocity (J2000) and BC of 
+%               multiple objects and reduced order density state
+%   AC          continuous-time state transition matrix
+%   BC          continuous-time input matrix
+%   SWinputs    Space weather inputs
+%   r           number of reduced order modes [integer]
+%   noo         number of objects [integer]
+%   svs         state size per object [integer]
 %   F_U         interpolant of gridded reduced-order modes
 %   M_U         interpolant of gridded mean density
 %   maxAtmAlt   maximum altitude of ROM density model
-%   et0         initial ephemeris time
+%   et0         initial ephemeris time (seconds since J2000 epoch)
 %   jdate0      initial Julian date
 %
 % Outputs:
@@ -26,79 +27,71 @@ function [ f ] = computeDerivative_PosVelBcRom(t,xp,AC,BC,Inp,rR,noo,svs,F_U,M_U
 % Massachusetts Institute of Technology, Dept. of Aeronautics and
 % Astronautics
 % email: davidgondelach@gmail.com
-% Sep 2019; Last revision: 24-Sep-2019
+% Sep 2019; Last revision: 03-Oct-2019
 
 %------------- BEGIN CODE --------------
 
-x = reshape(xp,svs*noo+rR,[]);
+% Convert state to single column
+x = reshape(xp,svs*noo+r,[]);
 
 % Date and time
-et = et0 + t;
-jdate = jdate0 + t / 86400;
-[yy, mm, dd, hh, mnmn, ss] = datevec(jdate-1721058.5);
-UThrs = hh + mnmn/60 + ss/3600;
+et = et0 + t; % Ephemeris time
+jdate = jdate0 + t / 86400; % Julian date
 
-% Space weather
-Inp = interp1(Inp(1,:),Inp(2:end,:)',jdate)';
+% Space weather inputs for current time
+SWinputs = interp1(SWinputs(1,:),SWinputs(2:end,:)',jdate)';
 
+% m = state vector lenght = noo*svs + r
+% n = number of states (i.e. number of sigma points in UKF)
 [m,n]=size(x);
+
+% State derivative f=dx/dt
 f=zeros(m,n);
 
-% Compute densities
-rho = zeros(noo,n);
+% Compute accelerations per object
 for i = 1:noo
-    [LON,LAT,ALT]=gc2gd(x(svs*(i-1)+1:svs*(i-1)+3,:)',yy,mm,dd,hh,mnmn,ss,0,0,0);
-    LON(LON>180) = LON(LON>180) - 360;
-    SLT = UThrs+LON/15;
-    SLT(SLT>24) = SLT(SLT>24)-24;SLT(SLT<0) = SLT(SLT<0)+24;
-    UhI = zeros(n,rR);
-    for j = 1:rR
-       UhI(:,j) = F_U{j}(SLT,LAT,ALT);
-    end
-    MI = M_U(SLT,LAT,ALT);
-    rho(i,:) = 10.^(sum(UhI'.*x(end-rR+1:end,:),1)+MI');
-    rho(i,ALT>maxAtmAlt) = 0;
-end
-
-b_star = zeros(noo,n);
-for i = 1:noo
-    
-    % Compute Magnitudes
-    r2=(x(svs*(i-1)+1,:).^2+x(svs*(i-1)+2,:).^2+x(svs*(i-1)+3,:).^2);
-    r=r2.^(1/2);
-    
-    % Ballistic coefficient (BC)
-    b_star(i,:) = x(svs*i,:);
     
     % Position and velocity in ECEF frame
-    xform = cspice_sxform('J2000', 'ITRF93', et );
-    x_ecef = xform*x(svs*(i-1)+1:svs*(i-1)+6,:);
-    rr_ecef = x_ecef(1:3,:);
-    vv_ecef = x_ecef(4:6,:);  
-    mag_v_ecef = sqrt( sum( vv_ecef.^2, 1 ));
+    xform = cspice_sxform('J2000', 'ITRF93', et ); % J2000 to ECEF transformation matrix
+    x_ecef = xform*x(svs*(i-1)+1:svs*(i-1)+6,:); % State in ECEF
+    rr_ecef = x_ecef(1:3,:); % Position in ECEF
+    vv_ecef = x_ecef(4:6,:); % Velocity in ECEF
+    mag_v_ecef = sqrt( sum( vv_ecef.^2, 1 )); % Magnitude of velocity in ECEF
     
-    % Gravitational accelerations
+    % Gravitational accelerations in ECEF [m/s^2]
     [accGrav_ecefx, accGrav_ecefy, accGrav_ecefz] = rungravitysphericalharmonic(rr_ecef'*1000);
     
-    % Accelerations in ECEF frame
-    aa_ecef(1,:) = accGrav_ecefx'/1000 - 1/2.*b_star(i,:).*rho(i,:).*mag_v_ecef.*vv_ecef(1,:);
-    aa_ecef(2,:) = accGrav_ecefy'/1000 - 1/2.*b_star(i,:).*rho(i,:).*mag_v_ecef.*vv_ecef(2,:);
-    aa_ecef(3,:) = accGrav_ecefz'/1000 - 1/2.*b_star(i,:).*rho(i,:).*mag_v_ecef.*vv_ecef(3,:);
+    % Atmospheric densities [kg/m^3]
+    % Position in J2000
+    pos = x(svs*(i-1)+1:svs*(i-1)+3,:); 
+    % Reduced order density state
+    romState = x(end-r+1:end,:); 
+    % Compute density using reduced-order density model [kg/m^3]
+    rho = getDensityROM(pos,jdate,romState,r,F_U,M_U,maxAtmAlt);
     
-    % Velocity in J2000 frame
+    % Ballistic coefficients (BC) [m^2/(1000kg)]
+    b_star = x(svs*i,:);
+    
+    % Accelerations in ECEF: Earth gravity + drag acceleration [km/s^2]
+    aa_ecef(1,:) = accGrav_ecefx'/1000 - 1/2.*b_star.*rho.*mag_v_ecef.*vv_ecef(1,:);
+    aa_ecef(2,:) = accGrav_ecefy'/1000 - 1/2.*b_star.*rho.*mag_v_ecef.*vv_ecef(2,:);
+    aa_ecef(3,:) = accGrav_ecefz'/1000 - 1/2.*b_star.*rho.*mag_v_ecef.*vv_ecef(3,:);
+    
+    % Velocities in J2000 frame [km/s]
     f(svs*(i-1)+1,:) = x(svs*(i-1)+4,:);
     f(svs*(i-1)+2,:) = x(svs*(i-1)+5,:);
     f(svs*(i-1)+3,:) = x(svs*(i-1)+6,:);
-    % Acceleration in J2000 frame
+    % Accelerations in J2000 frame [km/s^2]
     f(svs*(i-1)+4:svs*(i-1)+6,:) = xform(1:3,1:3)' * aa_ecef;
-    % BC
+    % BC time derivative is zero
     f(svs*(i-1)+7,:) = 0;
   
 end
 
-% Rate of change of reduced-order modes
-f(end-rR+1:end,:) = AC * x(end-rR+1:end,:) + BC * Inp;
+% Time derivative of reduced-order density state
+f(end-r+1:end,:) = AC * x(end-r+1:end,:) + BC * SWinputs;
 
+% Convert state derivative to single column
 f = reshape(f,[],1);
 
 end
---
+++ AstroFunctions/getDensityROM.m
@@ -0,0 +1,56 @@
+function [rho] = getDensityROM(pos,jdate,romState,r,F_U,M_U,maxAtmAlt)
+% getDensityROM - Compute density using reduced-order density model
+%
+% Syntax:  [rho] = getDensityROM(pos,jdate,romState,r,F_U,M_U,maxAtmAlt)
+%
+% Inputs:
+%   pos         position vectors in J2000
+%   jdate       Julian date
+%   romState    reduced-order density state
+%   r           number of reduced order modes [integer]
+%   F_U         interpolant of gridded reduced-order modes
+%   M_U         interpolant of gridded mean density
+%   maxAtmAlt   maximum altitude of ROM density model
+%
+% Outputs:
+%    rho        densities at positions giving by pos
+
+% Author: David Gondelach
+% Massachusetts Institute of Technology, Dept. of Aeronautics and
+% Astronautics
+% email: davidgondelach@gmail.com
+% Sep 2019; Last revision: 03-Oct-2019
+
+%------------- BEGIN CODE --------------
+
+% Number of position vectors
+n = size(pos,2);
+
+% Date and time
+[yy, mm, dd, hh, mnmn, ss] = datevec(jdate-1721058.5); % Year, month, day, hour, minute, seconds in UTC
+UThrs = hh + mnmn/60 + ss/3600; % Hour of day in UTC
+
+% Convert ECI position to longitude, latitude and altitude
+[lon,lat,alt]=gc2gd(pos',yy,mm,dd,hh,mnmn,ss,0,0,0);
+lon(lon>180) = lon(lon>180) - 360;
+
+% Local solar time
+lst = UThrs+lon/15;
+lst(lst>24) = lst(lst>24)-24;
+lst(lst<0) = lst(lst<0)+24;
+
+% Spatial modes
+UhI = zeros(n,r);
+for j = 1:r
+    UhI(:,j) = F_U{j}(lst,lat,alt);
+end
+% Mean density
+MI = M_U(lst,lat,alt);
+
+% Density
+rho = 10.^(sum(UhI'.*romState,1)+MI');
+rho(alt>maxAtmAlt) = 0;
+
+end
+
+%------------- END OF CODE --------------
