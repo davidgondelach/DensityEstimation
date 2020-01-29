64dda65b58d4a0d733687859f5e24b45bb7548f9
davidgondelach <davidgondelach@gmail.com>
2020-01-29 09:11:06 -0500

    Fix curl command to download TLEs

--- TLEdata/downloadTLEsForEstimation.m
+++ TLEdata/downloadTLEsForEstimation.m
@@ -8,23 +8,23 @@ function [objects] = downloadTLEsForEstimation(username, password, startYear, st
 
 %------------- BEGIN CODE --------------
 
-curlCmd = "curl -o TLEdata/estimationObjects.tle https://www.space-track.org/ajaxauth/login -d ";
-usernameCmd = strcat("'identity=",username);
-passwordCmd = strcat("&password=",password);
-tleQuery = "&query=https://www.space-track.org/basicspacedata/query/class/tle/";
-epochQuery = strcat("EPOCH/",int2str(startYear), '-', sprintf('%02d',startMonth), '-', sprintf('%02d',startDay), '--', sprintf('%02d',endYear), '-', sprintf('%02d',endMonth), '-', sprintf('%02d',endDay), '/');
-apogeeQuery = strcat("APOGEE/%3C",num2str(maxAlt),'/');
+curlCmd = 'curl -o TLEdata/estimationObjects.tle https://www.space-track.org/ajaxauth/login -d ';
+usernameCmd = strcat('"identity=',username);
+passwordCmd = strcat('&password=',password);
+tleQuery = '&query=https://www.space-track.org/basicspacedata/query/class/tle/';
+epochQuery = strcat('EPOCH/',int2str(startYear), '-', sprintf('%02d',startMonth), '-', sprintf('%02d',startDay), '--', sprintf('%02d',endYear), '-', sprintf('%02d',endMonth), '-', sprintf('%02d',endDay), '/');
+apogeeQuery = strcat('APOGEE/%3C',num2str(maxAlt),'/');
 
 if ~isempty(selectedObjects)
-    objectsQuery = strcat("NORAD_CAT_ID/",sprintf('%05d',selectedObjects(1)));
+    objectsQuery = strcat('NORAD_CAT_ID/',sprintf('%05d',selectedObjects(1)));
     for i=2:length(selectedObjects)
-        objectsQuery = strcat(objectsQuery,",%20",sprintf('%05d',selectedObjects(i)));
+        objectsQuery = strcat(objectsQuery,',%20',sprintf('%05d',selectedObjects(i)));
     end
-    objectsQuery = strcat(objectsQuery,"/");
+    objectsQuery = strcat(objectsQuery,'/');
 else
-    objectsQuery = "";
+    objectsQuery = '';
 end
-orderingQuery = "orderby/NORAD_CAT_ID%20asc/format/tle/'";
+orderingQuery = 'orderby/NORAD_CAT_ID%20asc/format/tle/"';
 TLEdwnlcmd = strcat(curlCmd, usernameCmd, passwordCmd, tleQuery, epochQuery, apogeeQuery, objectsQuery, orderingQuery);
 system(TLEdwnlcmd);
 
