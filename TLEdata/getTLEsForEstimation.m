2a9f564897cb690ceb750ea6a93d3006ab765a49
davidgondelach <davidgondelach@gmail.com>
2020-01-30 22:53:32 -0500

    Check if TLE file exists and if TLE data for all objects is available.

--- TLEdata/getTLEsForEstimation.m
+++ TLEdata/getTLEsForEstimation.m
@@ -1,4 +1,4 @@
-function [objects] = getTLEsForEstimation(startYear, startMonth, startDay, endYear, endMonth, endDay, selectedObjects)
+function [objects] = getTLEsForEstimation(startYear, startMonth, startDay, endYear, endMonth, endDay, selectedObjects, getTLEsFromSingleFile)
 %getTLEsForEstimation - Read TLE data from file
 %
 % Author: David Gondelach
@@ -12,25 +12,48 @@ relativeDir = 'TLEdata';
 
 selectedObjects = sort(selectedObjects);
 
-objects = struct('noradID',{},'satrecs',{});
-for i=1:length(selectedObjects)
-    noradIDstr = sprintf('%05d',selectedObjects(i));
-    try % Try to read a ready TLEsStruct from the hard drive to avoid time-consmuing parsing.
-        matFile = fullfile(relativeDir,strcat(noradIDstr,'.mat'));
-        matData = load( matFile ); % We already have a TLEsStruct saved there.
-        object = matData.object;
-    catch
-        filepath = fullfile(relativeDir,strcat(noradIDstr,'.tle'));
-        [object] = getTLEs(filepath);
-        save(matFile,'object');
+if getTLEsFromSingleFile
+    % Read TLEs from "estimationObjects.tle"
+    filename = fullfile(relativeDir,'estimationObjects.tle');
+    if ~isfile(filename)
+        error('TLE file estimationObjects.tle was not found in %s directory.', relativeDir);
     end
+    [objects] = getTLEs(filename);
+else
+    % Read TLEs from individual files named "[NORADID].tle"
+    objects = struct('noradID',{},'satrecs',{});
+    for i=1:length(selectedObjects)
+        noradIDstr = sprintf('%05d',selectedObjects(i));
+        filepath = fullfile(relativeDir,strcat(noradIDstr,'.tle'));
+        if ~isfile(filepath)
+            error('TLE file %s.tle was not found in %s directory.', noradIDstr, relativeDir);
+        end
+        [object] = getTLEs(filepath);
+        objects = [objects,object];
+    end
+end
+
+% Filter TLEs on date
+jdStart = juliandate(datetime(startYear, startMonth, startDay,0,0,0));
+jdEnd   = juliandate(datetime(endYear, endMonth, endDay,0,0,0));
+for i=1:length(selectedObjects)
+    % NORAD ID
+    ID = selectedObjects(i);
+    % Find object
+    index = find([objects.noradID]==ID);
+    if isempty(index)
+        error('No TLEs found for object %.0f.', ID);
+    end
+    % Get object's TLE data
+    object = objects(index);
     
-    jdStart = juliandate(datetime(startYear, startMonth, startDay,0,0,0));
-    jdEnd   = juliandate(datetime(endYear, endMonth, endDay,0,0,0));
+    % Filter TLEs on date
     firstTLE = find([object.satrecs.jdsatepoch]>=jdStart,1,'first');
     lastTLE  = find([object.satrecs.jdsatepoch]<=jdEnd,1,'last');
-    object.satrecs = object.satrecs(firstTLE:lastTLE);
-    objects = [objects,object];
+    if isempty(firstTLE) || isempty(lastTLE)
+        error('No TLEs found for object %.0f between %.0f-%.0f-%.0f and %.0f-%.0f-%.0f.', ID, startDay, startMonth, startYear, endDay, endMonth, endYear);
+    end
+    objects(index).satrecs = object.satrecs(firstTLE:lastTLE);
 end
 
 end
